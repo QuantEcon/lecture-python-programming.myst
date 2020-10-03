6206c0a0ed3228805cbfeb5217c594176f33be41
mmcky <mmcky@users.noreply.github.com>
2020-10-03 11:23:07 +0000

    deploy: 7b7bfa1553105e8de7ac4627f0ea1b5daec8168b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2020-Oct-2',
+    VERSION: '2020-Oct-3',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
